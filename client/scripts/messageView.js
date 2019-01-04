2049af842ec37572ba5e3cdbba327416e9494ecd
Steve Miller <ben.el.elyon@gmail.com>
2019-01-03 22:12:28 -0500

    Passed all chatroom behavior tests - able to render messages and rooms to the DOM

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,20 @@
 var MessageView = {
 
   render: _.template(`
-      <!--
       <div class="chat">
-        <div class="username"></div>
-        <div></div>
+        <div class="username"><%= username %></div>
+        <div><%= text %></div>
       </div>
-      -->
     `)
 
 };
+
+// <%= movieDirector %>
+
+/*
+{
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+*/
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -5,7 +5,31 @@ var MessagesView = {
   initialize: function() {
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    var html = MessageView.render(message);
+    MessagesView.$chats.append(html);
   }
 
 };
+
+
+
+
+
+
+/*
+    App.fetch(function() {
+      var html;
+      for (var i = 0; i < this.data.length; i++) {
+        html += MessageView.render(this.data[i]);
+      }
+      //$("#movies").append(html);
+      MessagesView.$chats.append(html);
+    });
+    /*
+      get all posts from parse
+      for each post..
+        run it through templater
+        append to variable
+        append variable to $chats
+*/

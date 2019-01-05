9d4db5457a461dede2935398a2ba11b34a03e453
Steve Miller <ben.el.elyon@gmail.com>
2019-01-05 11:45:27 -0500

    Complete bare minimum requirements

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -2,9 +2,19 @@ var Friends = {
 
   friendList: {},
 
-  toggleStatus: function() {
-    $(this).css('background-color', 'lightblue');
+  toggleStatus: function(friendName) {
+    // $(this).css('background-color', 'lightblue');
+
     // highlight all messages currently on screen from user
+    if (typeof friendName !== 'string') {
+      friendName = $( this ).text();
+    }
+    Friends.friendList[friendName] = true;
+    $('div.username:contains(' + friendName + ')').parent().css('background-color', 'lightblue');
+
+    // highlights all messages from users with name 'me'
+    //$("div.username:contains('me')").css('background-color', 'lightblue')
+
     // store user in friendList for future reference on other reloads (may be separate method to be used in refresh() and here)
   }
 
--- client/scripts/friendsView.js
+++ client/scripts/friendsView.js
@@ -2,6 +2,12 @@ var FriendsView = {
 
   initialize: function() {
     $('.username').on('click', Friends.toggleStatus);
+    for (var key in Friends.friendList) {
+      Friends.toggleStatus(key);
+    }
   }
 
 };
+
+// look through friendsList object
+// toggleStatus
